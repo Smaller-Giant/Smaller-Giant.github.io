4dd152e041106ad61c1681399bf03583cc73573c
Smaller-Giant <mylesboy26b@gmail.com>
2025-06-29 12:47:36 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
-function toggleMenu() {
-  const d = document.getElementById('dropdownMenu');
-  d.style.display = d.style.display === 'flex' ? 'none' : 'flex';
-}
 
-document.querySelectorAll('section').forEach(sec => {
-  const obs = new IntersectionObserver(e => {
-    if (e[0].isIntersecting) {
-      sec.classList.add('visible');
-      obs.disconnect();
-    }
-  }, { threshold: 0.2 });
-  obs.observe(sec);
-});
