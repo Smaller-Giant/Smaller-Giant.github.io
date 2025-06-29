d656035aa0f83281d0fb9134fc0dc983810553c7
Smaller-Giant <mylesboy26b@gmail.com>
2025-06-29 12:03:49 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+// index.js - handles mobile menu toggle on homepage
 
+const menuToggle = document.querySelector('.menu-toggle');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+menuToggle.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+});
